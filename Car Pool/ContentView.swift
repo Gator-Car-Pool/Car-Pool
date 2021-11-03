62ad4453c44cb3fe03b1a56d80b45f11f651b43e
James <jlibyyy@gmail.com>
2021-11-03 19:01:34 -0400

    changing codebase to swiftui

---
+++ Car Pool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Car Pool
+//
+//  Created by James  Luberisse on 11/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pods/Target Support Files/Pods-Car Pool-Car PoolUITests/Pods-Car Pool-Car PoolUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Car_Pool_Car_PoolUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Car_Pool_Car_PoolUITests
+@end
