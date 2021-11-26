6fca7ff39f01321316c0db24d0feef8b144343cd
j-jurado <73544267+j-jurado@users.noreply.github.com>
2021-11-26 18:29:59 -0500

    commit

--- Pods/Target Support Files/Pods-Car Pool-Car PoolUITests/Pods-Car Pool-Car PoolUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Car_Pool_Car_PoolUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Car_Pool_Car_PoolUITests
-@end
--- Pods/Target Support Files/Pods-Car PoolTests/Pods-Car PoolTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Car_PoolTests : NSObject
-@end
-@implementation PodsDummy_Pods_Car_PoolTests
-@end
