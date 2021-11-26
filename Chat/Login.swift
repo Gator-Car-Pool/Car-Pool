0896526e1102a70e52de26da792c0aebf7463c98
James <35617601+jliby@users.noreply.github.com>
2021-11-26 13:03:18 -0500

    Add files via upload

---
+++ Chat/Login.swift
@@ -0,0 +1,47 @@
+//
+//  Login.swift
+//  Car Pool
+//
+//  Created by Jesus Jurado on 11/15/21.
+//
+import SwiftUI
+
+struct Login: View {
+    @State var email = ""
+    @State var password = ""
+    @ObservedObject var sessionSession = SessionStore()
+
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Email", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+
+                Button(action: {
+                    sessionSession.signIn(email: email, password: password)
+                }, label: {
+                    Text("Login")
+                })
+
+                Button(action: {
+                    sessionSession.signUp(email: email, password: password)
+                }, label: {
+                    Text("Sign up")
+                })
+            }
+            .padding(.horizontal)
+            .navigationBarTitle("Welcome")
+        }
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
